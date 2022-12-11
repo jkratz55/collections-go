06e597bd4973ea6e83434719fbb2c127a1a04502
Joseph Kratz <4985721+jkratz55@users.noreply.github.com>
2022-12-11 16:22:44 -0500

    Implemented Stack

--- stack.go
+++ stack.go
@@ -1,5 +1,79 @@
 package collections
 
+import (
+	"errors"
+)
+
+var (
+	// ErrEmptyStack is a sentinel error value indicating the operation cannot
+	// be performed on an empty stack.
+	ErrEmptyStack = errors.New("stack is empty")
+)
+
+// Stack represents a last-in-first-out (LIFO) stack of elements.
+//
+// The zero-value of Stack is not usable. In order to properly initialize and use
+// Stack use the NewStack function.
 type Stack[T any] struct {
 	data []T
+}
+
+// NewStack creates and initializes a new empty Stack.
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{
+		data: make([]T, 0),
+	}
+}
+
+// Push pushes an element to the top of the stack.
+func (s *Stack[T]) Push(val T) {
+	s.data = append(s.data, val)
+}
+
+// Pop removes and returns the element at the top of the stack. In the event the
+// Stack is empty a non-nil error value of ErrEmptyStack is returned.
+func (s *Stack[T]) Pop() (val T, err error) {
+	if len(s.data) == 0 {
+		return val, ErrEmptyStack
+	}
+	val = s.data[len(s.data)-1]
+	s.data = s.data[:len(s.data)-1]
+	return val, nil
+}
+
+// Peek returns the element at the top of the stack but doesn't remove it. Peek
+// additionally returns a boolean indicating if a value was returned. If the stack
+// was empty false we be returned.
+func (s *Stack[T]) Peek() (val T, ok bool) {
+	if len(s.data) == 0 {
+		return val, false
+	}
+	val = s.data[len(s.data)-1]
+	return val, true
+}
+
+// Size returns the current size (number of elements) on the stack.
+func (s *Stack[T]) Size() int {
+	return len(s.data)
+}
+
+// Empty returns a boolean indicating if the stack is empty.
+func (s *Stack[T]) Empty() bool {
+	return len(s.data) == 0
+}
+
+// PopEach will pop each element off the stack one by one until the stack is empty.
+// As each element is popped off the stack the provided function is executed with
+// the popped off value.
+func (s *Stack[T]) PopEach(fn func(val T)) {
+	if len(s.data) == 0 {
+		return
+	}
+	for {
+		val, err := s.Pop()
+		if err != nil {
+			return
+		}
+		fn(val)
+	}
 }
--- stack_test.go
+++ stack_test.go
@@ -1,1 +1,94 @@
 package collections
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewStack(t *testing.T) {
+	stack := NewStack[string]()
+	assert.NotNil(t, stack)
+}
+
+func TestStack_Push(t *testing.T) {
+	stack := NewStack[string]()
+	stack.Push("World")
+	stack.Push("Hello")
+
+	expected := []string{"World", "Hello"}
+	assert.Equal(t, expected, stack.data)
+	assert.Equal(t, 2, len(stack.data))
+}
+
+func TestStack_Pop(t *testing.T) {
+	stack := NewStack[string]()
+	stack.data = append(stack.data, "World", "Hello")
+	expected := []string{"Hello", "World"}
+	actual := make([]string, 0)
+
+	val, err := stack.Pop()
+	assert.NoError(t, err)
+	actual = append(actual, val)
+
+	val, err = stack.Pop()
+	assert.NoError(t, err)
+	actual = append(actual, val)
+
+	val, err = stack.Pop()
+	assert.ErrorIs(t, err, ErrEmptyStack)
+
+	assert.Equal(t, expected, actual)
+	assert.Equal(t, 0, len(stack.data))
+}
+
+func TestStack_Peek(t *testing.T) {
+	stack := NewStack[string]()
+	stack.data = append(stack.data, "World", "Hello")
+
+	val, ok := stack.Peek()
+	assert.True(t, ok)
+	assert.Equal(t, "Hello", val)
+	assert.Equal(t, 2, len(stack.data))
+
+	val, ok = stack.Peek()
+	assert.True(t, ok)
+	assert.Equal(t, "Hello", val)
+	assert.Equal(t, 2, len(stack.data))
+
+	stack.data = make([]string, 0)
+	val, ok = stack.Peek()
+	assert.False(t, ok)
+	assert.Equal(t, 0, len(stack.data))
+}
+
+func TestStack_Size(t *testing.T) {
+	stack := NewStack[string]()
+	assert.Equal(t, 0, stack.Size())
+	stack.data = append(stack.data, "test")
+	assert.Equal(t, 1, stack.Size())
+	stack.data = append(stack.data, "test")
+	assert.Equal(t, 2, stack.Size())
+}
+
+func TestStack_Empty(t *testing.T) {
+	stack := NewStack[string]()
+	assert.True(t, stack.Empty())
+	stack.data = append(stack.data, "test")
+	assert.False(t, stack.Empty())
+}
+
+func TestStack_PopEach(t *testing.T) {
+	actual := make([]string, 0)
+	fn := func(val string) {
+		actual = append(actual, val)
+	}
+
+	stack := NewStack[string]()
+	stack.data = append(stack.data, "World", "Hello")
+
+	stack.PopEach(fn)
+
+	assert.Equal(t, 0, len(stack.data))
+	assert.Equal(t, []string{"Hello", "World"}, actual)
+}
